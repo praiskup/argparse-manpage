249fde88a3d1b8b4b7c241e1f29b683c3a46705e
Gabriele Giammatteo <gabriele.giammatteo@eng.it>
2017-07-08 14:11:37 +0200

    added minimal setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='optargs-manpage',
+    version='0.0.1',
+    url='https://github.com/gabrielegiammatteo/build_manpage',
+    license='Apache 2.0',
+    py_modules = ['build_manpage'],
+    author='Gabriele Giammatteo',
+    author_email='gabriele.giammatteo@eng.it',
+    description='',
+)
