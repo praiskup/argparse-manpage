1b3068e15a8ac6725e1d993c9f010d3600f08fe9
Stephen Finucane <stephen@that.guru>
2019-09-06 16:44:33 +0100

    setup.py: Use README for long description
    Some style nits are addressed along the way.
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>

--- setup.py
+++ setup.py
@@ -1,28 +1,29 @@
 import os
-import sys
-from setuptools import setup, find_packages
 
+from setuptools import setup, find_packages
 from setuptools.command.build_py import build_py
 from setuptools.command.install import install
-from build_manpages import __version__
-from build_manpages.build_manpages \
-    import build_manpages, get_build_py_cmd, get_install_cmd
 
-ld = """
-Generate manual page an automatic way from ArgumentParser object, so the manpage
-1:1 corresponds to the automatically generated --help output.  The manpage
-generator needs to known the location of the object, user can specify that by
-(a) the module name or corresponding python filename and (b) the object name or
-the function name which returns the object.  There's a limited support for
-(deprecated) optparse objects, too.
-"""
+from build_manpages import __version__
+from build_manpages.build_manpages import build_manpages
+from build_manpages.build_manpages import get_build_py_cmd
+from build_manpages.build_manpages import get_install_cmd
+
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_readme():
+    with open(os.path.join(ROOT_DIR, 'README.rst')) as fh:
+        return ''.join(fh.readlines())
+
 
 setup(
     name='argparse-manpage',
     version=__version__,
     url='https://github.com/praiskup/argparse-manpage',
     license='Apache 2.0',
-    py_modules = ['build_manpage'],
+    py_modules=['build_manpage'],
     author='Gabriele Giammatteo',
     author_email='gabriele.giammatteo@eng.it',
     maintainer='Pavel Raiskup',
@@ -30,8 +31,8 @@ setup(
     packages=find_packages(),
     scripts=['bin/argparse-manpage'],
     data_files=[("", ["LICENSE", "NEWS"])],
-    description='Build manual page from python\'s ArgumentParser object.',
-    long_description=ld,
+    description="Build manual page from python's ArgumentParser object.",
+    long_description=get_readme(),
     cmdclass={
         'build_manpages': build_manpages,
         'build_py': get_build_py_cmd(build_py),
