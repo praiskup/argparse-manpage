573ac9d44fef3c3b2623f69ef5e11c94cc4914d4
Ben Woodcroft <benjwoodcroft@gmail.com>
2021-05-21 06:02:03 +1000

    tests: Ease running via nosetests and of specific files.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,11 +1,19 @@
-from build_manpages.manpage import Manpage
-
+import unittest
+import os.path
+import sys
 import argparse
 
-class TestEscapes(object):
+sys.path = [os.path.join(os.path.dirname(os.path.realpath(__file__)),'..')]+sys.path
+from build_manpages.manpage import Manpage
+
+
+class TestEscapes(unittest.TestCase):
 
     def test_backslash_escape(self):
         parser = argparse.ArgumentParser('duh')
         parser.add_argument("--jej", help="c:\\something")
         man = Manpage(parser)
         assert 'c:\\\\something' in str(man).split('\n')
+
+if __name__ == "__main__":
+    unittest.main()
