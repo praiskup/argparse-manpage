f1ff57a5d04881c30d6eff2a91fc00c2a55c486a
Pavel Raiskup <praiskup@redhat.com>
2021-11-28 22:50:20 +0100

    Add underline() to non-default METAVAR arguments

--- build_manpages/manpage.py
+++ build_manpages/manpage.py
@@ -103,26 +103,24 @@ class _ManpageFormatter(HelpFormatter):
     def _format_action_invocation(self, action):
         if not action.option_strings:
             metavar, = self._metavar_formatter(action, action.dest)(1)
-            metavar = bold(metavar)
-            return metavar
+            return bold(metavar)
 
+        parts = []
+
+        # if the Optional doesn't take a value, format is:
+        #    -s, --long
+        if action.nargs == 0:
+            parts.extend(map(bold, action.option_strings))
+
+        # if the Optional takes a value, format is:
+        #    -s ARGS, --long ARGS
         else:
-            parts = []
-
-            # if the Optional doesn't take a value, format is:
-            #    -s, --long
-            if action.nargs == 0:
-                parts.extend(map(bold, action.option_strings))
-
-            # if the Optional takes a value, format is:
-            #    -s ARGS, --long ARGS
-            else:
-                default = underline(action.dest.upper())
-                args_string = self._format_args(action, default)
-                for option_string in action.option_strings:
-                    parts.append('{} {}'.format(bold(option_string),
-                                                args_string))
-            return ', '.join(parts)
+            default = action.dest.upper()
+            args_string = self._format_args(action, default)
+            for option_string in action.option_strings:
+                parts.append('{} {}'.format(bold(option_string),
+                                            underline(args_string)))
+        return ', '.join(parts)
 
 
     def _format_parser(self, parser, subcommand=None):
